b91d4944bf792f82641c0c8eb279bfb39c8a3d6c
Doug Woodward <doug@fourthpin.com>
2024-06-18 21:23:59 -0700

    just checking in

--- get_pcb.py
+++ get_pcb.py
@@ -6,6 +6,7 @@ import flax
 import flax.linen as nn
 import optax
 from flax.training import train_state
+from jax import tree_util
 
 
 def get_data(split="train"):
@@ -17,41 +18,86 @@ def get_data(split="train"):
     return dataset_jax
 
 
-class FPN(nn.Module):
-
-    levels: int = 3
+class full_conv_encoder(nn.Module):
     
+    latent_dim: int = 128
+
     @nn.compact
     def __call__(self, x):
-        x = nn.Conv(features=64, kernel_size=(3, 3), strides=(1, 1), padding='SAME')(x)
-        x = nn.max_pool(x, window_shape=(2, 2), strides=(2, 2))
-
-        # Feature pyramid levels
-        levels = []
-        x = nn.Conv(features=64, kernel_size=(3, 3), strides=(1, 1), padding='SAME')(x)
+        x = nn.Conv(features=8, kernel_size=(3,3))(x)
         x = nn.relu(x)
-        prior_level = x
-        for i in range(self.levels):
-            x = nn.Conv(features=64, kernel_size=(3, 3), strides=(1, 1), padding='SAME')(x)
-            l_out = nn.relu(x)
-            x = nn.ConvTranspose(l_out, kernel_size=(3,), padding='VALID')
-            x = jnp.concatenate(prior_level, axis=-1)
-            prior_level = l_out
-            levels.append(x)
-            x = nn.max_pool(x, window_shape=(2, 2), strides=(2, 2))
+        x = nn.max_pool(x, window_shape=(2,2), strides=(2,2))
+
+        x = nn.Conv(features=16, kernel_size=(3,3))(x)
+        x = nn.relu(x)
+        x = nn.max_pool(x, window_shape=(2,2), strides=(2,2))
+
+        x = nn.Conv(features=32, kernel_size=(3,3))(x)
+        x = nn.relu(x)
+        x = nn.max_pool(x, window_shape=(2,2), strides=(2,2))
+
+        x = x.reshape(x.shape[0], -1)  # Image grid to single feature vector
+        x = nn.Dense(features=self.latent_dim)(x)
+
+        return x
+
+
+class full_conv_decoder(nn.Module):
+
+    latent_dim: int = 128
+
+    @nn.compact
+    def __call__(self, x):
+        x = nn.Dense(features=self.latent_dim)(x)
+        x = nn.relu(x)
+
+        x = x.reshape(x.shape[0], 4, 4, -1)
+ 
+        x = nn.ConvTranspose(features=16, kernel_size=(3,3), strides=(2,2))(x)
+        x = nn.relu(x)
+
+        x = nn.ConvTranspose(features=32, kernel_size=(3,3), strides=(2,2))(x)
+        x = nn.relu(x)
+
+        x = nn.ConvTranspose(features=3, kernel_size=(3,3), strides=(2,2))(x)
+        x = nn.tanh(x)
 
         return x
 
 
 class InstanceSegmentationModel(nn.Module):
-    @nn.compact
-    def __call__(self, x):
-        x = FPN()(x)
-        x = nn.Conv(features=1, kernel_size=(7, 7), strides=(2, 2), padding='SAME')(x)
-        return x
 
+    def setup(self, latent_dim: int = 128):
+        self.encoder = full_conv_encoder(latent_dim)
+        self.decoder = full_conv_decoder(latent_dim)
+        self.out_layer = nn.Conv(features=1, kernel_size=(7, 7), strides=(2, 2), padding='SAME')
+
+
+    def __call__(self, x):
+        z = self.encoder(x)
+        x_hat = self.decoder(z)
+        
+        x_out = self.out_layer(x_hat)
+        return x_out
+
+
+def convert_to_segmentation_format(instance_dict):
+    # Extract relevant data from the dictionary
+    category_id = instance_dict["category"][0]
+    bbox = jnp.asarray(instance_dict["bbox"][0])
+    segmentation = jnp.asarray(instance_dict["segmentation"][0])
+
+    # Create a PyTree structure for the output
+    output = {
+        "category_id": category_id,
+        "bbox": bbox,
+        "segmentation": segmentation,
+    }
+
+    return tree_util.tree_flatten(output)[0]
 
 def iou_loss(y_pred, y_true):
+    print(y_true)
     intersection = jnp.sum(y_pred * y_true, axis=(1, 2))
     union = jnp.sum(y_pred + y_true, axis=(1, 2))
     iou = intersection / union
@@ -61,24 +107,25 @@ def calc_loss(state, params, batch):
     img = batch['image']
     objects = batch['segementation']
     pred = state.apply_fn(params, img)
+    print(batch['segmentation'])
     return iou_loss(pred, objects)
 
 @jax.jit
 def train_step(state, batch):
     grad_fn = jax.value_and_grad(iou_loss, argnums=1)
-    optimizer = optax.adam(learning_rate=1e-4)
-    y_pred = model.apply(params, x)
-    loss = iou_loss(y_pred, y)
-    grads = jax.grad(loss)(params)
-    updates, params = optimizer.update(grads, params)
+    y_pred = state.apply_fn(state.params, batch['image'])
+    loss = iou_loss(y_pred, convert_to_segmentation_format(batch['objects']))
+    grads = jax.grad(loss)(state.params)
+    updates, params = state.optimizer.update(grads, state.params)
     return params
 
-def train_loop(model, params, dataset, epochs=100):
+def train_loop(model_state, dataset, epochs=100):
+
     for epoch in range(100):
         for batch in dataset:
-            print(batch)
-            params = train_step(params, model, batch['image'], batch['segmentation'])   
-    return params
+            print(batch['objects'])
+            model_state.params = train_step(model_state, batch)   
+    return model_state
 
 if __name__ == "__main__":
 
@@ -92,12 +139,12 @@ if __name__ == "__main__":
     rng, inp_rng, init_rng = jax.random.split(rng, 3)
     # Initialize the model
     params = model.init(init_rng, exp_img)
-    print(params)
 
+    optimizer = optax.adam(learning_rate=1e-4)
     #  
     model_state = train_state.TrainState.create(apply_fn=model.apply,
                                             params=params,
                                             tx=optimizer)
 
 
-    out_params = train_loop(model, model_params, train_dataset, epochs=3)
+    out_params = train_loop(model_state, train_dataset, epochs=3)
--- jet_dataloader.py
+++ jet_dataloader.py
@@ -1,36 +1,62 @@
 import pyarrow as pa
 import tensorflow as tf
 from datasets import load_dataset
+import numpy as np
+import jax.numpy as jnp
 import jax
 
-# Load the HuggingFace dataset
-dataset = load_dataset("Illia56/Military-Aircraft-Detection")
-batch_size = 2
-# Access the individual datasets
-train_dataset = dataset["train"]
-print(dataset.keys())
-print(train_dataset)
-# eval_dataset = dataset["validation"]
-# Convert the HuggingFace dataset to a TensorFlow Dataset
-tf_train_dataset = train_dataset.to_tf_dataset(
-    columns=["image"],
-    # label_cols=["label_column"],
-    shuffle=True,
-    batch_size=batch_size,
-)
+def get_dataset():
+    # Load the HuggingFace dataset
+    dataset = load_dataset("Illia56/Military-Aircraft-Detection")
+    print(dataset)
+    batch_size = 2
+    # Access the individual datasets
+    train_dataset = dataset["train"]
+    # train_dataset = train_dataset.map(lambda x: tf.cast(x['image'], tf.float32))
+    # eval_dataset = dataset["validation"]
+    # Convert the HuggingFace dataset to a TensorFlow Dataset
+    # train_dataset = tf.data.Dataset.from_tensor_slices(dataset["train"]["image"])
+    #tf_train_dataset = train_dataset.to_tf_dataset(
+    #    columns=["image"],
+    #    # label_cols=["label_column"],
+    #    shuffle=True,
+    #    batch_size=batch_size,
+    #)
+    print("setup tf dataset")
+
+    def preprocess_images(examples):
+        # Decode and preprocess the images
+        #print(examples)
+        # images = [tf.cast(tf.io.decode_image(img, channels=3), tf.float32) for img in examples['image']]
+        # images = [tf.io.decode_image(np.array(img).astype(np.float32), channels=3) for img in examples['image']]
+        images = [np.array(img).astype(np.float32) for img in examples['image']]
+        #print("----------")
+        #print(images)
+        #images = [tf.cast(img, tf.float32) for img in images]
+        return {'image': images}
+    # def preprocess_fn(x):
+    #    return x
+    # Preprocess the data
+    train_dataset = train_dataset.map(preprocess_images, batched=True)
+
+    # Batch the data
+    #tf_train_dataset = tf_train_dataset.batch(batch_size)
+    train_dataset = train_dataset.batch(batch_size, drop_remainder=True).prefetch(tf.data.AUTOTUNE)
+    # Create a data loader for Flax
+    train_data_loader = iter(train_dataset)
+    
+    return train_data_loader
 
 
-def preprocess_fn(x):
-    return x
-# Preprocess the data
-#train_dataset = train_dataset.map(preprocess_fn)
-
-# Batch the data
-tf_train_dataset = tf_train_dataset.batch(batch_size)
-# Create a data loader for Flax
-train_data_loader = iter(tf_train_dataset)
-
-# Use the data loader in your Flax model
-for batch in train_data_loader:
-    batch = jax.device_put(batch)
+def train_loop():
+    train_data_loader = get_dataset() 
+    print("Dataset acquired!")
+    # Use the data loader in your Flax model
+    print(train_data_loader)
+    for batch in train_data_loader:
+    #    # batch = jax.device_put(batch)
+        print(batch)
+        break
     # Train or evaluate your model with the batch
+
+train_loop()
